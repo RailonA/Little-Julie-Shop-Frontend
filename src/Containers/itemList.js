4429802292dcc36351c5efbb6f57ebf2c03197d6
RailonA <railonacosta@gmail.com>
2021-11-07 18:02:15 -0500

    Add one image from cloudinary by cld.image

--- src/Containers/itemList.js
+++ src/Containers/itemList.js
@@ -1,34 +1,58 @@
 import { Card } from 'react-bootstrap';
+// import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Cloudinary } from '@cloudinary/url-gen';
+// import { Image } from 'cloudinary-react';
 
-const ItemList = ({ items }) => (
-  <div className="container-fluid flex-column justify-content-center">
-    {
-      items.map((items) => (
-        <Card
-          key={items.id}
-        >
-          <table className="  m-3 col-10">
-            <tbody className=" col-12">
-              <tr className=" m-3 ">
-                <div className="m-3 col-12 flex-container justify-content-center">
-                  <div className="col-12 d-flex justify-content-center">
-                    <p className=" col-8  d-flex justify-content-center serviceCategoryTitle p-2">{ items.id }</p>
-                    <p className=" col-8  d-flex justify-content-center serviceCategoryTitle p-2">{ items.itemName }</p>
-                    <p className=" col-8  d-flex justify-content-center serviceCategoryTitle p-2">{ items.itemDescription }</p>
-                    <p className=" col-8  d-flex justify-content-center serviceCategoryTitle p-2">{ items.itemPrice }</p>
+// import { AdvancedImage } from '@cloudinary/react';
+// active_storage.key
+const ItemList = ({ items }) => {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'lil-julie-shop',
+    },
+    url: {
+      secure: true, // force https, set to false to force http
+    },
+  });
+  const myImage = cld.image('p6a1m4z8avkvxt0gwhdssnnk6mbk');
+  const myURL = myImage.toURL();
 
+  return (
+    <div className="container-fluid flex-column justify-content-center">
+      {
+        items.map((items) => (
+          <Card
+            key={items.id}
+          >
+            <table className="  m-3 col-10">
+              <tbody className=" col-12">
+                <tr className=" m-3 ">
+                  <div className="m-3 col-12 flex-container justify-content-center">
+                    <div className="col-12 d-flex justify-content-center">
+                      <div className="col-12 d-flex justify-content-center">
+                        <p className=" col-8  d-flex justify-content-center serviceCategoryTitle p-2">{ items.id }</p>
+                        <p className=" col-8  d-flex justify-content-center serviceCategoryTitle p-2">{ items.itemName }</p>
+                        <p className=" col-8  d-flex justify-content-center serviceCategoryTitle p-2">{ items.itemDescription }</p>
+                        <p className=" col-8  d-flex justify-content-center serviceCategoryTitle p-2">{ items.itemPrice }</p>
+                        <img src={myURL} alt=" " />
+                        {/* <img key={items.id} src={items.itemPhoto.blob.key} alt=" " /> */}
+                        {/* <Image
+                          publicID={items.itemPhoto}
+                        /> */}
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </tr>
-            </tbody>
-            <hr className=" col-12 " />
-          </table>
-        </Card>
-      ))
-    }
-  </div>
-);
+                </tr>
+              </tbody>
+              <hr className=" col-12 " />
+            </table>
+          </Card>
+        ))
+      }
+    </div>
+  );
+};
 
 ItemList.propTypes = {
   items: PropTypes.arrayOf(PropTypes.array).isRequired,
