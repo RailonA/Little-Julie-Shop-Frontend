a0cbf7d53878ba922d2107850ec790c5d18b926f
RailonA <railonacosta@gmail.com>
2022-01-27 12:50:58 -0500

    Move all components to route page

--- src/Pages/homePage.js
+++ src/Pages/homePage.js
@@ -1,34 +1,20 @@
 /* eslint-disable max-len */
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
-import requestItemInfo, { requestCategoryInfo, requestShoppingCart } from '../Helpers/requests';
+import requestItemInfo, { requestCategoryInfo } from '../Helpers/requests';
 import ItemList from '../Containers/itemList';
 import LeftColumn from '../Containers/leftColumn';
 import '../Assets/styles/navBar.css';
 import '../Assets/styles/leftColumn.css';
 
 const HomePage = () => {
-  let itemId;
   const itemData = useSelector((state) => state.items);
   const categoryData = useSelector((state) => state.category);
   const [selectedCategory, setSelectedCategory] = useState('');
   const [styleSheet, setStyleSheet] = useState(false);
 
-  const shoppingCartData = useSelector((state) => state.shoppingCart);
-  const [selectedShoppingCartItem, setSelectedShoppingCartItem] = useState('');
-  const userData = useSelector((state) => state.currentUser);
-
   const dispatch = useDispatch();
 
-  const addToShoppingCart = (e) => {
-    itemId = e.target.value;
-    setSelectedShoppingCartItem(itemId);
-    requestShoppingCart(
-      dispatch, userData.id, selectedShoppingCartItem, userData.token,
-    );
-  };
-
-  console.log(shoppingCartData);
   const [selectedChildCategory, setSelectedChildCategory] = useState('');
 
   const filteredItems = (selectedChildCategory !== '') ? itemData.itemsCollection.filter((item) => item.categories_id === parseInt(selectedChildCategory, 10)) : itemData.itemsCollection;
@@ -78,7 +64,6 @@ const HomePage = () => {
               itemCard={itemCard}
               itemPhotoContainer={itemPhotoContainer}
               buyButton={buyButton}
-              addToShoppingCart={addToShoppingCart}
             />
           </div>
         </div>
